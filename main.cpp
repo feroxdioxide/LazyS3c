436d7684f71b5ff22193bdddb569f795e352403b
ms3c <63225928+ms3c@users.noreply.github.com>
2020-04-06 00:00:30 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,2 +1,35 @@
 #include <iostream>
 #include <cmath>
+  using namespace std;
+
+int main(){
+int num1, num2;
+char op;
+// user prompt
+  cout<<"Enter the first number: ";
+  cin>>num1;
+  cout<<"Enter operator: ";
+  cin>>op;
+  cout<<"Enter the second number: ";
+  cin>>num2;
+//Function to check the math operator 
+int results;
+  if(op == '+'){
+  results = num1 + num2;
+  }
+  else if(op == '-'){
+    resulus = num1 - num2;
+  }
+  else if(op == '/'){
+    results = num1 / num2;
+  }
+  else if(op == '*'){
+    results = num1 * num2;
+  }
+ else(){
+ cout<<"Invalid operator";
+ }
+//print results
+cout<<results
+return 0;
+}
